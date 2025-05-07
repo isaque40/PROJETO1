ac02ad4a7a459d3a23bf7af6458d1850bb860f9a
Isaque <isaquesergio01@gmail.com>
2025-05-07 08:44:37 -0300

    criando uma requisição post

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,11 +17,12 @@ namespace PROJETO1.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Sobre() 
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using PROJETO1.Repositorio;
 namespace PROJETO1.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        /*DECLARANDO UMA VARIÁVEL PRIVADA SOMENTE PARA LEITURA DO TIPO UsuarioRepositorio
+        chamada _usuarioRepositorio*/
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        /*DEFININDO O CONSTRUTOR DA CLASSE USUARIOCONTROOLLER QUE VAI RECEBER UMA
+         INSTANCIA DE UsuarioRepositorio*/ 
+
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)//injeção de dependencia 
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            return View();
+        }
+
+        public IActionResult Contato() 
         {
             return View();
         }
     }
+    
+
 }
